01d49ab3bab0bfbbabc303d96eb8bd8311428e96
nodlya <nadya-popova-02@mail.ru>
2022-12-02 22:30:35 +0500

    component header

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from '@Assets/images/logo.png';
+import phone from '@Assets/images/phone.png';
+
+import './styles.css';
+
+const Header = () => (
+<header className='header'>
+            <div className='header-left'>
+                <img className='header__logo-png' src={logo}/>
+                <p className='header__text-info header__light-text'>Курьерская служба доставки</p>
+            </div>
+            <div className='header-left'>
+                <img className='header__phone-png' src={phone}/>
+                <a href='tel:+79090231251' className='header__phone header__light-text'>8 (909) 023-12-51</a>
+                <button className='header__enter-button'>Вход</button>
+            </div>
+</header>);
+
+export default Header;
--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
-
-import cat from '@Assets/images/cat.jpg';
+import Header from '@Components/header'
 
 import './styles.css';
 
 const Main = () => (
     <div className="main-page">
-        <h1 className="main-page__title">Главная</h1>
-        <img className="main-page__cat-img" src={cat} />
+        <Header/>
     </div>
 );
 
